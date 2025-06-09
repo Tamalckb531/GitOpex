0e9f83979a671d002f9f239568505ec945f8b3c4
Tamal <ckbtamaldipnew@gmail.com>
2025-06-10 01:49:48 +0600

    still stuffs not working

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,9 +1,10 @@
+import { addGitOpexButton } from "./helper/content";
+
 function App() {
+  addGitOpexButton();
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-red-500">
-        Hello world!
-      </h1>
+      <h1 className="text-3xl font-bold underline text-red-500">Hello Tamal</h1>
     </div>
   );
 }
