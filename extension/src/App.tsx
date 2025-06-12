9c6cf1874858abe516fea257d42d4bf8cdf8d59d
Tamal <ckbtamaldipnew@gmail.com>
2025-06-13 02:04:52 +0600

    UI -> Fixed chat bar in bottom

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -5,10 +5,12 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)]">
-      <Header />
-      <AMA />
-      <ChatBox />
+    <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)] flex flex-col justify-between">
+      <div>
+        <Header />
+        <AMA />
+        <ChatBox />
+      </div>
       <ChatBar />
     </div>
   );
