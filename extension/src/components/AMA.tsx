a6f8668c761a10907904d777c0d68bbf12e66b4e
Tamal <ckbtamaldipnew@gmail.com>
2025-06-13 00:32:30 +0600

    UI -> add AMA section

--- extension/src/components/AMA.tsx
+++ extension/src/components/AMA.tsx
@@ -1,5 +1,12 @@
 const AMA = () => {
-  return <div>AMA</div>;
+  return (
+    <div className=" flex flex-col justify-center items-center gap-1 p-2 mt-2 text-[var(--dark-color)]">
+      <p className=" text-sm font-bold">
+        We are now at Github Profile : Tamalckb531
+      </p>
+      <p>Ask Me Anything</p>
+    </div>
+  );
 };
 
 export default AMA;
