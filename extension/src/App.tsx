38624f8f7af588ed81a21fa641d2dd8bde677db5
Tamal <ckbtamaldipnew@gmail.com>
2025-06-12 23:53:20 +0600

    UI -> add css var

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className=" w-[400px] h-[600px]">
+    <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)]">
       <Header />
       <AMA />
       <ChatBox />
