2f02c9ecc6020e4a4ff08b273ea2ffce2db55489
Tamal <ckbtamaldipnew@gmail.com>
2025-06-13 00:26:04 +0600

    add Header section

--- extension/src/components/Header.tsx
+++ extension/src/components/Header.tsx
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className=" flex flex-col items-center justify-center p-2 gap-1 text-[var(--dark-color)] shadow ">
+      <h1 className=" text-2xl font-bold">GitOpex</h1>
+      <p className=" italic">Your Personal Github Assistant</p>
+    </div>
+  );
 };
 
 export default Header;
