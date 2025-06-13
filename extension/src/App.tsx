38b7168a3b41a52c43f6e505e3ad0812cbefc4fa
Tamal <ckbtamaldipnew@gmail.com>
2025-06-13 20:51:18 +0600

    add scroll bar to chatbox

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -6,7 +6,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)] flex flex-col justify-between">
-      <div className=" h-full">
+      <div className="flex flex-col flex-grow overflow-hidden">
         <Header />
         <AMA />
         <ChatBox />
--- extension/src/components/ChatBox.tsx
+++ extension/src/components/ChatBox.tsx
@@ -4,7 +4,7 @@ interface msg {
 }
 const ChatBox = () => {
   return (
-    <div className=" flex flex-col gap-4 mx-2 w-full ">
+    <div className=" flex flex-col flex-grow gap-4 mx-2 w-full overflow-y-auto scrollbar-thin">
       <ChatMsg
         type="send"
         message="Hey! Can You please explain what kind of projects this profile has ?"
@@ -13,6 +13,11 @@ const ChatBox = () => {
         type="receive"
         message="Hi there! This profile has mostly Typescript based projects. Two of them (rankdevs, GitOpex) are actively receiving open source contributions."
       />
+      <ChatMsg
+        type="send"
+        message="Okay! Can you tell me where should I start contributing ? I use typescript and pro-efficient at web dev"
+      />
+      <ChatMsg type="receive" message="Rankdevs will be perfect for you." />
       <ChatMsg
         type="send"
         message="Okay! Can you tell me where should I start contributing ? I use typescript and pro-efficient at web dev"
