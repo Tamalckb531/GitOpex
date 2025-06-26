47a779964eaf2cdb27493f02e62f06c4f108ecf5
Tamal <ckbtamaldipnew@gmail.com>
2025-06-27 02:22:12 +0600

    extension -> add background tab listener

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,4 +1,22 @@
+import { getGitHubPageType } from "../helpers/func";
 import { isGithubUrl, scrapeGitHubProfile } from "./background.core";
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== "complete") return;
+
+  const url = tab.url || "";
+  if (!isGithubUrl(url)) return;
+
+  const pageType = getGitHubPageType(url);
+
+  if (pageType == "PROFILE") {
+    chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_PROFILE" });
+  }
+
+  if (pageType == "REPO" || pageType == "REPO_IN") {
+    chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO" });
+  }
+});
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   const url = sender.tab?.url || "";
