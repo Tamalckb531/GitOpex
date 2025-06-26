806459f29633e91fab2fd24a7803140b65c838e5
Tamal <ckbtamaldipnew@gmail.com>
2025-06-26 21:55:22 +0600

    server -> add cors policy

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,18 @@
 import { Hono } from "hono";
 import dataRoute from "./routes/data.route";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+//? CORS policy
+app.use(
+  "*",
+  cors({
+    origin: "*",
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/health", (c) => {
   return c.json({ message: "server is healthy" });
