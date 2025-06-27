32db3e1829c075bb2c0cabdd731896cadb78cec9
Tamal <ckbtamaldipnew@gmail.com>
2025-06-27 16:40:51 +0600

    extension -> add the popup logic for trigger the scraping

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -1,9 +1,20 @@
+import { useEffect } from "react";
 import AMA from "./components/AMA";
 import ChatBar from "./components/ChatBar";
 import ChatBox from "./components/ChatBox";
 import Header from "./components/Header";
 
 function App() {
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const url = tabs[0].url || "";
+      chrome.runtime.sendMessage({
+        type: "INIT_SCRAPE",
+        url,
+        tabId: tabs[0].id,
+      });
+    });
+  }, []);
   return (
     <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)] flex flex-col justify-between">
       <div className="flex flex-col flex-grow overflow-hidden">
--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -1,26 +1,24 @@
 import { getGitHubPageType } from "../helpers/func";
 import { isGithubUrl, scrapeGitHubProfile } from "./background.core";
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status !== "complete") return;
-
-  const url = tab.url || "";
-  if (!isGithubUrl(url)) return;
-
-  const pageType = getGitHubPageType(url);
-
-  if (pageType == "PROFILE") {
-    chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_PROFILE" });
-  }
-
-  if (pageType == "REPO" || pageType == "REPO_IN") {
-    chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO" });
-  }
-});
-
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   const url = sender.tab?.url || "";
   if (!isGithubUrl(url)) return;
+
+  if (message.type === "INIT_SCRAPE") {
+    const { url, tabId } = message;
+    if (!isGithubUrl(url)) return;
+
+    const pageType = getGitHubPageType(url);
+
+    if (pageType == "PROFILE") {
+      chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_PROFILE" });
+    }
+
+    if (pageType == "REPO" || pageType == "REPO_IN") {
+      chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO" });
+    }
+  }
 
   if (message.type === "GITHUB_PROFILE_DATA") {
     const enriched = await scrapeGitHubProfile(message.payload);
