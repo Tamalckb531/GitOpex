f6d85726c3bcc39cf7439ac1fe6c26cf73f66e29
Tamal <ckbtamaldipnew@gmail.com>
2025-07-01 14:58:21 +0600

    server -> add more stuff in insert service stringifyEnriched functino

--- server/src/services/insert.service.ts
+++ server/src/services/insert.service.ts
@@ -2,17 +2,35 @@ import { Enriched } from "../types/data.type";
 import { storeEmbeddings } from "../vector/embed";
 
 const stringifyEnriched = (enriched: Enriched): string[] => {
-  const { userData, allRepos } = enriched;
+  const { userData, allRepos, activeRepos, popularOSRepos } = enriched;
 
   const profileText = `Name: ${userData.name}, Username: ${userData.username}, Bio: ${userData.bio}, Location: ${userData.location}, Website: ${userData.website}`;
 
   const repoTexts = allRepos.map((repo) => {
-    return `Repo: ${repo.name}, Description: ${repo.description}, Language: ${
-      repo.language
-    }, Stars: ${repo.stargazers_count}, Topics: ${repo.topics?.join(", ")}`;
+    return `Repository: ${repo.name}, Description: ${
+      repo.description
+    }, Language: ${repo.language}, Stars: ${
+      repo.stargazers_count
+    }, Topics: ${repo.topics?.join(", ")}`;
   });
 
-  return [profileText, ...repoTexts];
+  const activeRepoTexts = activeRepos.map((repo) => {
+    return `Active Repository: ${repo.name}, Description: ${
+      repo.description
+    }, Language: ${repo.language}, Stars: ${
+      repo.stargazers_count
+    }, Topics: ${repo.topics?.join(", ")}`;
+  });
+
+  const popularOsTexts = popularOSRepos.map((repo) => {
+    return `Popular Open Source Repo: ${repo.name}, Description: ${
+      repo.description
+    }, Language: ${repo.language}, Stars: ${
+      repo.stargazers_count
+    }, Topics: ${repo.topics?.join(", ")}`;
+  });
+
+  return [profileText, ...repoTexts, ...activeRepoTexts, ...popularOsTexts];
 };
 
 export const handleEnrichedData = async (enriched: Enriched) => {
