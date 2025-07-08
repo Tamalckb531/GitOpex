2cc6caa804f8bebee1ae77c9293e3ebabf57cc2f
Tamal <ckbtamaldipnew@gmail.com>
2025-07-09 01:48:01 +0600

    server -> RAG -> add generation node

--- server/src/agents/generation.node.ts
+++ server/src/agents/generation.node.ts
@@ -1,14 +1,40 @@
 import { RunnableConfig } from "@langchain/core/runnables";
 import { GraphState } from "./state";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { AI_API_KEY } from "../config/config";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { formatDocumentsAsString } from "langchain/util/document";
 
 export const generate = async (
   state: typeof GraphState.State,
-  _config?: RunnableConfig
+  config?: RunnableConfig
 ): Promise<Partial<typeof GraphState.State>> => {
-  console.log("🧠 Generating final output...");
+  console.log("Generating final output...");
 
-  // TODO: Use LLM to generate response using state.documents + state.question
+  const model = new ChatGoogleGenerativeAI({
+    apiKey: AI_API_KEY,
+    model: "gemini-1.5-flash",
+    temperature: 0,
+  });
+
+  //? Generate a well written prompt for RAG work
+  const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+  //? Feed the prompt to the model and create pipeline to parse the string output from complex ai output
+  const ragChain = prompt.pipe(model).pipe(new StringOutputParser());
+
+  const generation = await ragChain
+    .withConfig({ runName: "GenerateAnswer" })
+    .invoke(
+      {
+        context: formatDocumentsAsString(state.documents),
+        question: state.question,
+      },
+      config
+    );
+
   return {
-    generation: "This is the final answer.",
+    generation,
   };
 };
