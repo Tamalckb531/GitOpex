e1cf26e321985b59bcca4786cbc350d78c24240b
Tamal <ckbtamaldipnew@gmail.com>
2025-07-09 00:35:57 +0600

    server -> RAG -> add retrieval node

--- server/src/agents/retrieval.node.ts
+++ server/src/agents/retrieval.node.ts
@@ -1,16 +1,25 @@
 import { RunnableConfig } from "@langchain/core/runnables";
 import { GraphState } from "./state";
+import { ScoreThresholdRetriever } from "langchain/retrievers/score_threshold";
+import { vectorStore } from "../vector/embed";
 
 export const retrieve = async (
   state: typeof GraphState.State,
-  _config?: RunnableConfig
+  config?: RunnableConfig
 ): Promise<Partial<typeof GraphState.State>> => {
-  console.log("🔍 Retrieving from vector store...");
+  console.log("Retrieving from vector store...");
 
-  // TODO: Insert your retrieval logic here using state.question
-  // and return matching documents from vector DB
+  const retriever = ScoreThresholdRetriever.fromVectorStore(vectorStore, {
+    minSimilarityScore: 0.3,
+    maxK: 1,
+    kIncrement: 1,
+  });
+
+  const relatedDocuments = await retriever
+    .withConfig({ runName: "FetchRelevantDocuments" })
+    .invoke(state.question, config);
 
   return {
-    documents: [], // dummy for now
+    documents: relatedDocuments,
   };
 };
