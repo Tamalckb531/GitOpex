619d9515414bfccd862ebd781b4355c304a07dfb
Tamal <ckbtamaldipnew@gmail.com>
2025-07-22 01:35:11 +0600

    server -> add pinecone database in embed.ts

--- server/src/vector/embed.ts
+++ server/src/vector/embed.ts
@@ -1,26 +1,56 @@
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
-import { Document } from "langchain/document";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { Pinecone } from "@pinecone-database/pinecone";
 
-export let vectorStore: any;
-
 //TODO: have to implement pinecone here
-export const storeEmbeddings = async (docs: string[], apiKey: string) => {
-  const pc = new Pinecone({
-    apiKey: apiKey,
-  });
+export const storeEmbeddings = async (
+  docs: string[],
+  apiKey: string,
+  pineconeKey: string,
+  info: string
+) => {
   const embeddings = new GoogleGenerativeAIEmbeddings({
     apiKey: apiKey,
     model: "models/embedding-001",
   });
 
-  //? One in memory variable that stores all data for users
-  vectorStore = new MemoryVectorStore(embeddings);
+  //? Generate vector embeddings from input docs
+  const vectors = await embeddings.embedDocuments(docs);
 
-  const documents = docs.map(
-    (text: any) => new Document({ pageContent: text })
-  );
+  //? Format each vector with metadata nd unique ID
+  const now = new Date().toISOString();
+  const upsertData = vectors.map((values, i) => ({
+    id: `doc-${i}-${Date.now()}`,
+    values,
+    metadata: {
+      info,
+      date: now,
+      text: docs[i],
+    },
+  }));
 
-  await vectorStore.addDocuments(documents);
+  //? Init Pinecone
+  const pinecone = new Pinecone({ apiKey: pineconeKey });
+  const indexName = "gitopex-index";
+  const index = pinecone.Index(indexName);
+
+  //? Create index if it doesn't exist
+  const indexList = await pinecone.listIndexes();
+  const indexNames = indexList.indexes?.map((i) => i.name);
+
+  if (indexNames && !indexNames.includes(indexName)) {
+    await pinecone.createIndex({
+      name: indexName,
+      dimension: vectors[0].length,
+      metric: "cosine",
+      spec: {
+        serverless: {
+          region: "us-east-1",
+          cloud: "aws",
+        },
+      },
+    });
+  }
+
+  //? Upsert vector data into pinecone database
+  await index.upsert(upsertData);
 };
