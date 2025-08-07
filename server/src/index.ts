4e4ef62ff18b2c7d194d56b0411911f58f38c9ce
Tamal <ckbtamaldipnew@gmail.com>
2025-08-08 00:03:41 +0600

    server -> error handling issue in extension solved from backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import dataRoute from "./routes/data.route";
 import authRoute from "./routes/auth.route";
 import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
 
 // ?Create the main Hono app
 const app = new Hono<{
@@ -23,6 +24,13 @@ app.use(
     allowHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    return c.json({ message: err.message }, err.status);
+  }
+  return c.json({ message: "Internal Server Error" }, 500);
+});
 
 app.get("/health", (c) => {
   return c.json({ message: "server is healthy" });
