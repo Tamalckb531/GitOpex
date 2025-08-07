b1327e79b7b1c098b804f4f07d8a5627809296f2
Tamal <ckbtamaldipnew@gmail.com>
2025-08-08 00:36:02 +0600

    extension -> Extension now render chat when having the token

--- extension/src/App.tsx
+++ extension/src/App.tsx
@@ -5,6 +5,7 @@ import { TabContext } from "./context/TabContext";
 import LoginWrapper from "./components/Wrapper/LoginWrapper";
 import SignupWrapper from "./components/Wrapper/SignupWrapper";
 import SettingsWrapper from "./components/Wrapper/SettingsWrapper";
+import { Storage } from "./types/data.type";
 // import { isGithubUrl } from "./helpers/func";
 
 function App() {
@@ -24,7 +25,11 @@ function App() {
   const context = useContext(TabContext);
   if (!context) return null;
 
-  const { tab } = context;
+  const { tab, setTab } = context;
+
+  const token = localStorage.getItem(Storage.AUTH);
+
+  if (token) setTab("main");
 
   return (
     <div className=" w-[400px] h-[600px]  bg-[var(--bg-color)] flex flex-col justify-between">
