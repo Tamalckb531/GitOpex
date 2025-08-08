c9eac592c8265ae061d3ce4d4d7ea2486ffe1e47
Tamal <ckbtamaldipnew@gmail.com>
2025-08-08 13:15:59 +0600

    extension -> add all chrome Scrape case in background

--- extension/src/background/background.ts
+++ extension/src/background/background.ts
@@ -6,16 +6,21 @@ chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
 
     const pageType = getGitHubPageType(url);
 
-    if (pageType == "PROFILE") {
-      chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_PROFILE" });
-    }
-
-    if (
-      pageType == "REPO" ||
-      pageType == "REPO_IN_Folder" ||
-      pageType == "REPO_IN_File"
-    ) {
-      chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO" });
+    switch (pageType) {
+      case "PROFILE":
+        chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_PROFILE" });
+        break;
+      case "REPO":
+        chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO" });
+        break;
+      case "REPO_IN_Folder":
+        chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO_Folder" });
+        break;
+      case "REPO_IN_File":
+        chrome.tabs.sendMessage(tabId, { type: "START_SCRAPE_REPO_File" });
+        break;
+      default:
+        break;
     }
   }
 
