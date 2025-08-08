d73b6e7c16a22ebec141578c8fbf1e2fd767fda4
Tamal <ckbtamaldipnew@gmail.com>
2025-08-08 13:27:02 +0600

    extension -> add clean switch case structure for content.ts code

--- extension/src/content/content.ts
+++ extension/src/content/content.ts
@@ -7,12 +7,22 @@ if (!window.location.hostname.includes("github.com")) {
 }
 
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.type === ChromeTypes.PROFILE) {
-    const data = getProfileData();
-    //? Sending profile data to background.ts
-    chrome.runtime.sendMessage({
-      type: ChromeTypes.GT_PROF_DATA,
-      payload: data,
-    });
+  switch (message.type) {
+    case ChromeTypes.PROFILE:
+      const data = getProfileData();
+      //? Sending profile data to background.ts
+      chrome.runtime.sendMessage({
+        type: ChromeTypes.GT_PROF_DATA,
+        payload: data,
+      });
+      break;
+    case ChromeTypes.REPO:
+      break;
+    case ChromeTypes.REPO_FOLDER:
+      break;
+    case ChromeTypes.REPO_FILE:
+      break;
+    default:
+      console.warn("Unknown message type:", message.type);
   }
 });
