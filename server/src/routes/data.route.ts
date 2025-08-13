d602e6997246995c8a5416ddfffb4ce0c0192a17
Tamal <ckbtamaldipnew@gmail.com>
2025-08-13 20:10:15 +0600

    server -> add the route for rag all types data

--- server/src/routes/data.route.ts
+++ server/src/routes/data.route.ts
@@ -4,7 +4,11 @@ import { authMiddleware } from "../middleware/tokenDetector";
 
 const dataRoute = new Hono();
 
-dataRoute.post("/rag", authMiddleware, insertData);
+dataRoute.post("/rag/profile", authMiddleware, insertData);
+dataRoute.post("/rag/repo", authMiddleware, insertData);
+dataRoute.post("/rag/repo_file", authMiddleware, insertData);
+dataRoute.post("/rag/repo_folder", authMiddleware, insertData);
+dataRoute.post("/check", authMiddleware, insertData);
 dataRoute.post("/query", authMiddleware, invokeAgent);
 
 export default dataRoute;
