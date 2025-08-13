88fb475d96aac6e0fcbff47dc349f9dff9ed333d
Tamal <ckbtamaldipnew@gmail.com>
2025-08-14 00:05:21 +0600

    server -> add all controller function

--- server/src/controller/data.controller.ts
+++ server/src/controller/data.controller.ts
@@ -2,7 +2,83 @@ import { Context } from "hono";
 import { handleEnrichedData, handleInvoking } from "../services/insert.service";
 import { Enriched, Query } from "../types/data.type";
 
-export const insertData = async (c: Context) => {
+export const insertDataProfile = async (c: Context) => {
+  const enriched: Enriched = await c.req.json();
+  const userId: string | null = c.get("userId");
+  const apiKey: string = c.env.AI_API_KEY;
+  const encryptKey: string = c.env.ENCRYPTION_KEY;
+  const pineconeKey: string = c.env.PINECONE_API_KEY;
+  const db_url: string = c.env.DATABASE_URL;
+
+  await handleEnrichedData(
+    enriched,
+    userId,
+    apiKey,
+    encryptKey,
+    pineconeKey,
+    db_url
+  );
+
+  return c.json({ status: "ok" });
+};
+export const insertDataRepo = async (c: Context) => {
+  const enriched: Enriched = await c.req.json();
+  const userId: string | null = c.get("userId");
+  const apiKey: string = c.env.AI_API_KEY;
+  const encryptKey: string = c.env.ENCRYPTION_KEY;
+  const pineconeKey: string = c.env.PINECONE_API_KEY;
+  const db_url: string = c.env.DATABASE_URL;
+
+  await handleEnrichedData(
+    enriched,
+    userId,
+    apiKey,
+    encryptKey,
+    pineconeKey,
+    db_url
+  );
+
+  return c.json({ status: "ok" });
+};
+export const insertDataRepoFile = async (c: Context) => {
+  const enriched: Enriched = await c.req.json();
+  const userId: string | null = c.get("userId");
+  const apiKey: string = c.env.AI_API_KEY;
+  const encryptKey: string = c.env.ENCRYPTION_KEY;
+  const pineconeKey: string = c.env.PINECONE_API_KEY;
+  const db_url: string = c.env.DATABASE_URL;
+
+  await handleEnrichedData(
+    enriched,
+    userId,
+    apiKey,
+    encryptKey,
+    pineconeKey,
+    db_url
+  );
+
+  return c.json({ status: "ok" });
+};
+export const insertDataRepoFolder = async (c: Context) => {
+  const enriched: Enriched = await c.req.json();
+  const userId: string | null = c.get("userId");
+  const apiKey: string = c.env.AI_API_KEY;
+  const encryptKey: string = c.env.ENCRYPTION_KEY;
+  const pineconeKey: string = c.env.PINECONE_API_KEY;
+  const db_url: string = c.env.DATABASE_URL;
+
+  await handleEnrichedData(
+    enriched,
+    userId,
+    apiKey,
+    encryptKey,
+    pineconeKey,
+    db_url
+  );
+
+  return c.json({ status: "ok" });
+};
+export const checker = async (c: Context) => {
   const enriched: Enriched = await c.req.json();
   const userId: string | null = c.get("userId");
   const apiKey: string = c.env.AI_API_KEY;
--- server/src/routes/data.route.ts
+++ server/src/routes/data.route.ts
@@ -1,14 +1,21 @@
 import { Hono } from "hono";
-import { insertData, invokeAgent } from "../controller/data.controller";
+import {
+  checker,
+  insertDataProfile,
+  insertDataRepo,
+  insertDataRepoFile,
+  insertDataRepoFolder,
+  invokeAgent,
+} from "../controller/data.controller";
 import { authMiddleware } from "../middleware/tokenDetector";
 
 const dataRoute = new Hono();
 
-dataRoute.post("/rag/profile", authMiddleware, insertData);
-dataRoute.post("/rag/repo", authMiddleware, insertData);
-dataRoute.post("/rag/repo_file", authMiddleware, insertData);
-dataRoute.post("/rag/repo_folder", authMiddleware, insertData);
-dataRoute.post("/check", authMiddleware, insertData);
+dataRoute.post("/rag/profile", authMiddleware, insertDataProfile);
+dataRoute.post("/rag/repo", authMiddleware, insertDataRepo);
+dataRoute.post("/rag/repo_file", authMiddleware, insertDataRepoFile);
+dataRoute.post("/rag/repo_folder", authMiddleware, insertDataRepoFolder);
+dataRoute.post("/check", authMiddleware, checker);
 dataRoute.post("/query", authMiddleware, invokeAgent);
 
 export default dataRoute;
